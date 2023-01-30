6721fc137aa58e0aac8d83641eb2a56a83835fd2
Reza Rahimloo <mr.rezarahimlou@gmail.com>
2023-01-30 22:43:27 +0330

    Navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
 
 import Home from "./routes/home/home.component";
+
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>I am the navigation bar</h1>
+      </div>
+      <Outlet/>
+    </div>
+  )
+}
+
+const Shop = () => (<h1>Let's Shop</h1>);
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/Ho" element={<Home/>} />
+      <Route path="/" element={<Navigation/>} >
+        <Route index={true} element={<Home/>} />
+        <Route path="shop" element={<Shop/>}/>
+      </Route>
     </Routes>
   );
 }
